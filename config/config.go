1ad6461f5290cbdb9483f865262ffd71ef03930d
GLAZERadr <adrianbadjideh@student.telkomuniversity.ac.id>
2023-08-13 19:34:52 +0800

    working on docker containerization

--- config/config.go
+++ config/config.go
@@ -3,65 +3,39 @@ package config
 import (
 	"os"
 	"log"
-
-	"github.com/joho/godotenv"
 )
 
 //function to take host value from .env file
 func ConfigHost() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	return os.Getenv("HOST")
 }
 
 //function to take port value from .env file
 func ConfigPort() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	return os.Getenv("PORT")
 }
 
 //function to take mongodb url value from .env file
 func ConfigDB() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	return os.Getenv("DATABASE_URL")
 }
 
 func ConfigDBname() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	return os.Getenv("DATABASE_NAME")
 }
 
 //function to take openai api key value from .env file
 func ConfigOpenAI() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	return os.Getenv("OPENAI_API_KEY")
 }
 
 //function to take jwt key value from .env file
 func ConfigJWTkey() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	return os.Getenv("JWT_KEY")
 }
